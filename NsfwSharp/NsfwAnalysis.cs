fe00a98d5358f35fae0f7ad767cb20fc65cca68b
Gabriele Bologna <bolognagabriele2003@gmail.com>
2025-02-06 23:32:42 +0100

    First commit

---
+++ NsfwSharp/NsfwAnalysis.cs
@@ -0,0 +1,20 @@
+﻿using SkiaSharp;
+
+namespace NsfwSharp
+{
+    public class NsfwAnalysis
+    {
+        public bool IsNsfw { get; private set; }
+        public NsfwDetection[] Detections { get; private set; }
+        public SKImage OriginalImage { get; private set; }
+        public SKImage DetectionsImage { get; private set; }
+
+        public NsfwAnalysis(bool isNsfw, NsfwDetection[] detections, SKImage originalImage, SKImage detectionsImage)
+        {
+            IsNsfw = isNsfw;
+            Detections = detections;
+            OriginalImage = originalImage;
+            DetectionsImage = detectionsImage;
+        }
+    }
+}
